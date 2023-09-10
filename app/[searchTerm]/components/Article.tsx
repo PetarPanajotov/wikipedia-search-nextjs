b42da1b903231ad4acbba797b0a9200cedfb67c5
Petar Panayotov <dakona98@gmail.com>
2023-09-10 16:24:22 +0300

    seperated article from page

---
+++ app/[searchTerm]/components/Article.tsx
@@ -0,0 +1,23 @@
+type Props = {
+    result: Article
+}
+
+export default function Article({result}: Props) {
+    return (
+        <div key={result.pageId} className="flex gap-2 text-white px-2 py-4">
+            {result.thumbnail &&
+                <img
+                    src={result.thumbnail?.source}
+                    alt=""
+                    width={result.thumbnail?.width}
+                    height={result.thumbnail?.height}
+                    loading="lazy"
+                />
+            }
+            <div className="self-center">
+                <h2 className="text-2xl underline">{result.title}</h2>
+                <p>{result.extract}</p>
+            </div>
+        </div>
+    );
+};
