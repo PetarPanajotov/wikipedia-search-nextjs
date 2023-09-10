d2381748ee816c171fa4de4f13e92900bf35f621
Petar Panayotov <dakona98@gmail.com>
2023-09-10 16:24:33 +0300

    added generatemetadata

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,24 +1,32 @@
 import getWikiResults from "@/lib/getWikiResults";
+import Article from "./components/Article";
+
+export async function generateMetadata({ params: { searchTerm } }: Props) {
+    const data: SearchResult = await getWikiResults(searchTerm);
+    const displayTerm = searchTerm.replaceAll('%20', ' ') //replace URL white spaces
+    if (!data.query?.pages) {
+        return {
+            title: `${displayTerm} Not Found`
+        }
+    }
+    return {
+        title: displayTerm,
+        description: `Search result for ${displayTerm}`
+    };
+};
 
 export default async function SearchResults({ params: { searchTerm } }: Props) {
     const data: SearchResult = await getWikiResults(searchTerm);
     const results: Article[] | undefined = data?.query?.pages;
-    console.log(results)
     return (
-        <>
-            <main className="container mx-auto px-12 text-lg flex flex-col gap-6">
-                {results ? (
-                    Object.values(results).map((result) => (
-                        <div key={result.pageId} className="flex gap-2 border-t-4 text-white">
-                            <img src={result.thumbnail?.source} alt="" width={result.thumbnail?.width} height={result.thumbnail?.height}/>
-                            <p className="self-center">{result.title}</p>
-                            <p>{result.extract}</p>
-                        </div>
-                    ))
-                ) : (
-                    <p>No results...</p>
-                )}
-            </main>
-        </>
+        <main className="max-w-3xl container mx-auto flex flex-col bg-slate-500 justify-center">
+            {results ? (
+                Object.values(results).map((result: Article) => (
+                    <Article key={result.pageId} result={result} />
+                ))
+            ) : (
+                <p>No results...</p>
+            )}
+        </main>
     );
-}
+};
